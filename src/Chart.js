c9beac1eda09f6744d80de43c1ee1128bcd6aa70
Welington Manfrim <welington.manfrim@kumulus.com>
2023-07-24 11:27:18 -0300

    feat(chart): Anotaçãoes de fibonacci baseada no maior e no menor valor do gráfico no período selecionado

--- src/Chart.js
+++ src/Chart.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import ApexCharts from 'react-apexcharts';
 import { subDays, subMonths } from 'date-fns';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import './index.css';
 
 const CandlestickChart = () => {
   const [data, setData] = useState([]);
   const [apiData, setApiData] = useState([]);
   const [period, setPeriod] = useState('1d'); // Default period is 1 day
   const [loading, setLoading] = useState(true);
+  const [showAnnotations, setShowAnnotations] = useState(true);
 
   const fetchData = async () => {
     setLoading(true);
     try {
-      const apiKey = 'BIVV2WO1A01YPAJO';
-      const fromSymbol = 'USD'; // Símbolo para dólar
-      const toSymbol = 'BRL'; // Símbolo para real brasileiro
+      const apiKey = 'G1AAZYJWPM8KLMHM';
+      const fromSymbol = 'USD';
+      const toSymbol = 'BRL';
       const apiUrl = `https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=${fromSymbol}&to_symbol=${toSymbol}&apikey=${apiKey}`;
 
-      // Realizar a requisição à API usando fetch()
       const response = await fetch(apiUrl);
       const responseData = await response.json();
 
       if (responseData['Time Series FX (Daily)']) {
-        // Extrair os dados da resposta
         const timeSeries = responseData['Time Series FX (Daily)'];
 
-        // Converter os dados para o formato adequado para o gráfico
         const chartData = [];
         for (let date in timeSeries) {
           chartData.push({
@@ -38,13 +38,11 @@ const CandlestickChart = () => {
           });
         }
 
-        // Ordenar os dados pela data crescente
         chartData.sort((a, b) => new Date(a.x) - new Date(b.x));
 
         setData(chartData);
         setApiData(chartData);
       } else {
-        // If no data found, handle it here (e.g., show an error message)
         setData([]);
       }
 
@@ -63,7 +61,6 @@ const CandlestickChart = () => {
   const handleButtonClick = (period) => {
     setPeriod(period);
 
-    // Filtrar os dados existentes com base no período selecionado
     const today = new Date();
     let startDate;
     switch (period) {
@@ -85,9 +82,114 @@ const CandlestickChart = () => {
         break;
     }
 
-    // Filtrar os dados com base no intervalo de datas
     const filteredData = apiData.filter((item) => new Date(item.x) >= startDate);
     setData(filteredData);
+  };
+
+  const minY = Math.min(...data.map((item) => item.y[2])); 
+  const maxY = Math.max(...data.map((item) => item.y[1])); 
+
+  const annotationObject = [
+    {
+      y: minY,
+      y2: minY + 0.236 * (maxY - minY),
+      borderColor: "#000",
+      fillColor: "#f87171",
+      opacity: 0.2,
+      label: {
+        borderColor: "black",
+        style: {
+          fontSize: "10px",
+          color: "white",
+          background: "black"
+        },
+        text: "0.236"
+      }
+    },
+    {
+      y: minY + 0.236 * (maxY - minY),
+      y2: minY + 0.382 * (maxY - minY),
+      borderColor: "#000",
+      fillColor: "#FEB019",
+      opacity: 0.2,
+      label: {
+        borderColor: "black",
+        style: {
+          fontSize: "10px",
+          color: "white",
+          background: "black"
+        },
+        text: "0.382"
+      }
+    },
+    {
+      y: minY + 0.382 * (maxY - minY),
+      y2: minY + 0.5 * (maxY - minY),
+      borderColor: "#000",
+      fillColor: "#4ade80",
+      opacity: 0.2,
+      label: {
+        borderColor: "black",
+        style: {
+          fontSize: "10px",
+          color: "white",
+          background: "black"
+        },
+        text: "0.5"
+      }
+    },
+    {
+      y: minY + 0.5 * (maxY - minY),
+      y2: minY + 0.618 * (maxY - minY),
+      borderColor: "#000",
+      fillColor: "#22d3ee",
+      opacity: 0.2,
+      label: {
+        borderColor: "black",
+        style: {
+          fontSize: "10px",
+          color: "white",
+          background: "black"
+        },
+        text: "0.618"
+      }
+    },
+    {
+      y: minY + 0.618 * (maxY - minY),
+      y2: minY + 0.786 * (maxY - minY),
+      borderColor: "#000",
+      fillColor: "#e879f9",
+      opacity: 0.2,
+      label: {
+        borderColor: "black",
+        style: {
+          fontSize: "10px",
+          color: "white",
+          background: "black"
+        },
+        text: "0.786"
+      }
+    },
+    {
+      y: minY + 0.786 * (maxY - minY),
+      y2: maxY,
+      borderColor: "#000",
+      fillColor: "#a78bfa",
+      opacity: 0.2,
+      label: {
+        borderColor: "black",
+        style: {
+          fontSize: "10px",
+          color: "white",
+          background: "black"
+        },
+        text: "1"
+      }
+    }
+  ]
+
+  const handleToggleAnnotations = () => {
+    setShowAnnotations(!showAnnotations);
   };
 
   return (
@@ -98,12 +200,18 @@ const CandlestickChart = () => {
       justifyContent: "center",
       alignItems: "center"
     }}>
-      <div>
-        <button onClick={() => handleButtonClick('1m')}>1 mês</button>
-        <button onClick={() => handleButtonClick('15d')}>15 dias</button>
-        <button onClick={() => handleButtonClick('1w')}>1 semana</button>
-        <button onClick={() => handleButtonClick('1d')}>1 dia</button>
-        <button onClick={() => handleButtonClick('all')}>Todos os dias</button>
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+      }}>
+        <button onClick={() => handleButtonClick('all')} className='btn'>100 dias</button>
+        <button onClick={() => handleButtonClick('1m')} className='btn'>1 mês</button>
+        <button onClick={() => handleButtonClick('15d')} className='btn'>15 dias</button>
+        <button onClick={() => handleButtonClick('1w')} className='btn'>1 semana</button>
+        <button onClick={() => handleButtonClick('1d')} className='btn'>1 dia</button>
+        <button onClick={handleToggleAnnotations} className='btn'>
+          {showAnnotations ? <FaEye size={20}/> : <FaEyeSlash size={20}/>}
+        </button>
       </div>
 
       {loading ? (
@@ -119,6 +227,17 @@ const CandlestickChart = () => {
           options={{
             xaxis: {
               type: 'datetime',
+            },
+            plotOptions: {
+              candlestick: {
+                colors: {
+                  upward: '#10b981',
+                  downward: '#ef4444'
+                }
+              }
+            },
+            annotations: {
+              yaxis: showAnnotations ? annotationObject : [], 
             },
           }}
           height={600}
